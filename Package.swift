1f29b6be0d3f76f9c775e827e388a19c4869c1bd
priyal <priyal@devineglobe.com>
2026-06-15 17:13:42 +0530

    path changes

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .target(
             name: "SwiftBlocks",
-            path: "Sources/SwiftBlocks"
+            path: "SwiftBlocks/Sources/SwiftBlocks" 
         )
     ]
 )
